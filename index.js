a49625761cbefe625fd27592aa350473846411cf
Adarsh9-dev <adarshpriyadarshi001@gmail.com>
2023-04-19 08:50:24 +0530

    change

--- index.js
+++ index.js
@@ -18,12 +18,7 @@ app.use(cors());
 
 mongoose.set('strictQuery', true);
 //Mongodb Connection
-mongoose.connect("mongodb+srv://Adarsh:qEXLiMWjB4Vvy6y7@cluster0.sf8bwna.mongodb.net/chat_application?retryWrites=true&w=majority",{
-    //must add in order to not get any error masseges:
-    useUnifiedTopology:true,
-    useNewUrlParser: true,
-    useCreateIndex: true
-})
+mongoose.connect("mongodb+srv://Adarsh:qEXLiMWjB4Vvy6y7@cluster0.sf8bwna.mongodb.net/chat_application?retryWrites=true&w=majority")
 .then(()=>console.log("DB Connected"))
 .catch((err)=>console.log("DB Not Connected",err))
 
